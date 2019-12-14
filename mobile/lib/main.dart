bc6c202eeeae8cea7e05c1575db1bb46cfff5a10
Sumant Raj <sumant261195@gmail.com>
2019-12-15 02:32:16 +0530

    cards stacking change

---
+++ mobile/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'homepage.dart';
+import 'login_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: FirstPage(),
+    );
+  }
+}
